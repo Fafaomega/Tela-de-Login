9e700c8ecf1c0b62f8d83ea9d2351ef137c6cbce
fafaomega <sadopikasdpok@outlook.com>
2023-06-09 17:12:56 -0300

    Tela de login

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-const usuario = document.querySelector('#usuario');
+const usuario = document.querySelector('.usuario');
 const password = document.querySelector('#senha');
 
 const login = document.querySelector('.login');
 const cadastrar = document.querySelector('.conta-nova');
 
-function limpaBarra (){
+function limpaBarra() {
     usuario.value = '';
     password.value = '';
     usuario.focus();
@@ -13,40 +13,63 @@ function limpaBarra (){
 let saveEmail = [];
 let saveSenha = [];
 
+// login.addEventListener('keypress', (e) => {
+//     if(e.keycode === 13){
+//         alert('a');
+//     }
+// })
+
 login.addEventListener('click', (e) => {
 
+    const mensagemErro = document.querySelector('.mensagem');
     const email = usuario.value;
     const senha = password.value;
 
-    if (saveEmail.includes(email) && saveSenha.includes(senha)){
+    if (saveEmail.includes(email) && saveSenha.includes(senha)) {
         window.location.href = 'logado.html';
     } else {
-        alert("Email ou senha inseridos incorretos!");
+        mensagemErro.innerHTML = 'Email ou Senha inválidos!'
+        mensagemErro.classList.remove('certo');
+        usuario.classList.add('errado');
     }
+
     limpaBarra();
 });
 
 cadastrar.addEventListener('click', (e) => {
 
+    const mensagemDeCadastro = document.querySelector('.mensagem');
     const email = usuario.value;
     const senha = password.value;
 
-    saveEmail = JSON.parse(localStorage.getItem("salvarEmail")) || []; 
-    saveSenha = JSON.parse(localStorage.getItem("salvarSenha")) || [];
-
-    if (saveEmail.includes(email)){
-        alert("Email ja registrado!");
-        limpaBarra();
-        return;
+    if (email == '' || senha == '') {
+        mensagemDeCadastro.classList.remove('certo');
+        mensagemDeCadastro.innerHTML = 'Dados inseridos inválidos!';
     };
 
-    saveEmail.push(email);
-    saveSenha.push(senha);
+    if (email != '' && senha != '') {
+        
+        saveEmail = JSON.parse(localStorage.getItem("salvarEmail")) || [];
+        saveSenha = JSON.parse(localStorage.getItem("salvarSenha")) || [];
 
-    localStorage.setItem("salvarEmail", JSON.stringify(saveEmail));
-    localStorage.setItem("salvarSenha", JSON.stringify(saveSenha));
+        if (saveEmail.includes(email)) {
+            mensagemDeCadastro.classList.remove('certo');
+            mensagemDeCadastro.innerHTML = 'Email ja registrado!';
+            limpaBarra();
+            return;
+        };
 
-    alert("Conta criada com sucesso!");
+        saveEmail.push(email);
+        saveSenha.push(senha);
+
+        localStorage.setItem("salvarEmail", JSON.stringify(saveEmail));
+        localStorage.setItem("salvarSenha", JSON.stringify(saveSenha));
+
+        mensagemDeCadastro.classList.add('certo');
+        mensagemDeCadastro.innerHTML = 'Conta Criado com sucesso';
+        limpaBarra();
+    };
+
 
     limpaBarra();
 });
